6cb5f974e2d57218b630a73ca18cb6b56d9524ff
fabioNog <fabio.nogueira.carmo@gmail.com>
2024-05-26 18:50:46 -0300

    Remove package lock and modify page build card and install yarn lock

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,20 @@
+import { Card, CardDescription, CardHeader, CardTitle,CardContent, CardFooter } from "@/components/ui/card"
+
 export default function Home() {
   return (
-    <h1>Hello Word</h1>
+    <div className="flex items-center justify-center">
+      <Card className="w-[400px] h-[700px]">
+        <CardHeader>
+          <CardTitle>My Chat English</CardTitle>
+          <CardDescription>Using Vercel SDK to Create a chat bot</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Mensagens</p>
+        </CardContent>
+        <CardFooter>
+          form
+        </CardFooter>
+      </Card>
+    </div>
   );
 }
