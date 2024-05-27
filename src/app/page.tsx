2d0bd392a7e332e22135ac6aa6bd8b8deaed9076
fabioNog <fabio.nogueira.carmo@gmail.com>
2024-05-26 21:01:06 -0300

    Finish chat conversation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,48 +1,87 @@
+"use client";
+
+import { useState } from 'react';
 import { Button } from "@/components/ui/button";
 import { Card, CardDescription, CardHeader, CardTitle, CardContent, CardFooter } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
+interface Message {
+  role: string;
+  content: string;
+}
+
 export default function Home() {
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [inputText, setInputText] = useState("");
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    
+    const newMessage: Message = {
+      role: "user",
+      content: inputText
+    };
+
+    // Add user message to messages
+    setMessages((prevMessages) => [...prevMessages, newMessage]);
+    setInputText("");
+
+    try {
+      const response = await fetch('http://localhost:3001/chatgpt/generate', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ prompt: inputText })
+      });
+
+      const data = await response.json();
+      console.log("Mensagem da API:", data);
+      const botMessage: Message = {
+        role: "bot",
+        content: data.candidates[0].content.parts[0].text
+      };
+
+      // Add bot response to messages
+      setMessages((prevMessages) => [...prevMessages, botMessage]);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
   return (
-    <div className="flex min-h-screen bg-slate-50 items-center justify-center">
-      <Card className="w-[440px] h-[700px] grid grid-rows-[min-content_1fr_min-content]">
+    <div className="flex min-h-screen bg-slate-50 items-center justify-center p-4">
+      <Card className="w-[440px] h-[700px] grid grid-rows-[min-content_1fr_min-content] shadow-lg">
         <CardHeader>
           <CardTitle>My Chat English</CardTitle>
           <CardDescription>Using Vercel SDK to Create a chat bot</CardDescription>
         </CardHeader>
-        <CardContent className="space-y-4">
-          <div className="flex gap-3 text-slate-600 text-sm">
-            <Avatar>
-              <AvatarFallback>FB</AvatarFallback>
-              <AvatarImage src="https://github.com/fabioNog.png"/>
-            </Avatar>
-            <p className="leading-relaxed">
-  <span className="block font-bold text-slate-700">Humano</span>
-  {`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,!`}
-</p>
-
-
-
-          </div>
-
-          <div className="flex gap-3 text-slate-600 text-sm">
-            <Avatar>
-              <AvatarFallback>FB</AvatarFallback>
-              <AvatarImage src="https://fabionog.github.io/fabionogueira/static/media/fabionogueira-pc.1806dcda3d1feef28a3b.jpeg"/>
-            </Avatar>
-            <p className="leading-relaxed">
-  <span className="block font-bold text-slate-700">Humano</span>
-  {`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,!`}
-</p>
-
-
-
-          </div>
+        <CardContent className="space-y-4 overflow-auto">
+          {messages.map((message, index) => (
+            <div key={index} className="flex gap-3 text-slate-600 text-sm">
+              <Avatar>
+                <AvatarFallback>{message.role === "user" ? "U" : "B"}</AvatarFallback>
+                <AvatarImage src={message.role === "user" ? "https://github.com/fabioNog.png" : "https://fabionog.github.io/fabionogueira/static/media/fabionogueira-pc.1806dcda3d1feef28a3b.jpeg"} />
+              </Avatar>
+              <p className="leading-relaxed">
+                <span className="block font-bold text-slate-700">{message.role === "user" ? "Humano" : "Bot"}</span>
+                {message.content}
+              </p>
+            </div>
+          ))}
         </CardContent>
-        <CardFooter className="space-x-2">
-         <Input placeholder="How Can I help you?" className="flex-1"/>
-         <Button type="submit">Send</Button>
+        <CardFooter>
+          <form onSubmit={handleSubmit} className="w-full flex gap-3">
+            <Input 
+              placeholder="How Can I help you?" 
+              id="messageInput" 
+              className="flex-1 h-12" 
+              value={inputText} 
+              onChange={(e) => setInputText(e.target.value)} 
+            />
+            <Button type="submit">Send</Button>
+          </form>
         </CardFooter>
       </Card>
     </div>
